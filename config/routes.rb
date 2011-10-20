605a67c7e348beee59cba479e14d5ca299b33d96
Casey Harford <casey@caseyharford.com>
2011-10-19 23:00:45 -0700

    fixed bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
   resources :users;
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
- # get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
