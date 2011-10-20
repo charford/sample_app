aef61d084d3bee4ed45793ef45effc552826b355
Casey Harford <casey@caseyharford.com>
2011-10-19 23:04:37 -0700

    added validation to password_confirmation, felt this was necessary even though not in tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => { :within => 6..40 }
+  validates :password_confirmation, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
   before_save :encrypt_password
   
   def has_password?(submitted_password)
